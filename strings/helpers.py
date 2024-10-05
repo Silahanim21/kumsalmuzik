a0495ef150bd390da8911fef4b1c829a5bc7b79c
Silahanim21 <turkcell63632@gmail.com>
2024-10-06 01:47:43 +0300

    Add files via upload

---
+++ strings/helpers.py
@@ -0,0 +1,144 @@
+HELP_1 = """
+ ADMİN KOMUTLARI:
+ᴀᴅᴍɪɴ ᴋᴏᴍᴜᴛʟᴀʀı :
+
+╰☞ /durdur /pause - Akışı duraklatır.
+╰☞ /devam /resume - Akışı devam ettirir.
+╰☞ /son - /end - Akışı Sonlandırır.
+╰☞ /atla /skip - Diğer parça ya atlar.
+╰☞ /ileri - Oynatılan parçayı ileri alır.
+╰☞ /gerial - Oynatılan parçayı geri alır.
+╰☞ /karistir /suffle- Sıraya alınan Parçaları karışık oynatır.
+╰☞ /tekrarla /loop - oynatılan parçayı istediğiniz kadar tekrar eder.
+╰☞ /reload - Admin Önbelleğini yeniler.
+/reboot - Botu yeniden başlatır..
+"""
+HELP_2 = """
+
+etiketleme komutları:
+.
+
+╰☞ /utag - grubunuzdaki üyeleri etiketler.
+╰☞ /atag - yöneticileri etiketler.
+╰☞ /dur - etiketlemeyi durdurur
+HELP_3 = """
+**YAYIN YAPMA ÖZELLİĞİ**
+
+REKLAM KOMUTLARI
+🔸/reklam [Mesaj veya Mesaja Cevap] - Herhangi bir mesajı Bot'un Sunulan Sohbetlerine yayınlayın.
+
+yayın seçenekleri:
+**-pin** : Bu, mesajınızı sabitleyecektir 
+**-pinloud** : Bu, mesajınızı yüksek sesli bildirimle sabitleyecektir
+**-user** : Bu, mesajınızı botunuzu başlatan kullanıcılara yayınlayacaktır.
+**-assistant** : Bu, mesajınızı botunuzun asistan hesabından yayınlayacaktır.
+**-nobot** : Bu, botunuzu mesaj yayınlamamaya zorlar
+
+**Örnek:** `/reklam -user -assistant -pin MERHABA `
+"""
+HELP_4 = """
+ʙʟᴀᴄᴋʟɪsᴛ ᴋᴏᴍᴜᴛʟᴀʀı : [ᴏɴʟʏ ғᴏʀ sᴜᴅᴏᴇʀs]
+
+ʀᴇsᴛʀɪᴄᴛ sʜɪᴛ ᴄʜᴀᴛs ᴛᴏ ᴜsᴇ ᴏᴜʀ ᴘʀᴇᴄɪᴏᴜs ʙᴏᴛ.
+
+/blacklistchat [SOHBET KİMLİĞİ] : BOT KULLANARAK SOHBETLERİ KARA LİSTE EDİN..
+/whitelistchat [SOHBET KİMLİĞİ] : KARA LİSTELENEN SOHBETİ BEYAZ LİSTEYE ALIN..
+/blacklistedchat : ᴋᴀʀᴀ ʟɪsᴛᴇʏᴇ ᴀʟıɴᴀɴ ɢʀᴜᴘʟᴀʀ.
+"""
+HELP_5 = """
+
+ʙʟᴏᴄᴋ ᴜsᴇʀs: [ᴏɴʟʏ ғᴏʀ sᴜᴅᴏᴇʀs]
+
+sᴛᴀʀᴛs ɪɢɴᴏʀɪɴɢ ᴛʜᴇ ʙʟᴀᴄᴋʟɪsᴛᴇᴅ ᴜsᴇʀ, sᴏ ᴛʜᴀᴛ ʜᴇ ᴄᴀɴ'ᴛ ᴜsᴇ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs.
+
+🔸/block [Kullanıcı adı veya bir kullanıcıya yanıt] - Bir kullanıcının bot komutlarını kullanmasını engeller.
+🔸/unblock [Kullanıcı adı veya bir kullanıcıya yanıt] - Bir kullanıcıyı Bot'un Engellenenler Listesinden çıkarın.
+🔸/blocklist - Engellenen Kullanıcı Listelerini Kontrol Edin
+"""
+HELP_6 = """
+
+ᴄʜᴀɴɴᴇʟ ᴩʟᴀʏ ᴄᴏᴍᴍᴀɴᴅs:
+
+ʏᴏᴜ ᴄᴀɴ sᴛʀᴇᴀᴍ ᴀᴜᴅɪᴏ/ᴠɪᴅᴇᴏ ɪɴ ᴄʜᴀɴɴᴇʟ.
+
+/cplay - /coynat: sᴛᴀʀᴛs sᴛʀᴇᴀᴍɪɴɢ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴀᴜᴅɪᴏ ᴛʀᴀᴄᴋ ᴏɴ ᴄʜᴀɴɴᴇʟ's ᴠɪᴅᴇᴏᴄʜᴀᴛ.
+/cvplay - /cvoynat: sᴛᴀʀᴛs sᴛʀᴇᴀᴍɪɴɢ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴠɪᴅᴇᴏ ᴛʀᴀᴄᴋ ᴏɴ ᴄʜᴀɴɴᴇʟ's ᴠɪᴅᴇᴏᴄʜᴀᴛ.
+/cplayforce or /cvplayforce : sᴛᴏᴩs ᴛʜᴇ ᴏɴɢᴏɪɴɢ sᴛʀᴇᴀᴍ ᴀɴᴅ sᴛᴀʀᴛs sᴛʀᴇᴀᴍɪɴɢ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴛʀᴀᴄᴋ.
+
+/channelplay [ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ɪᴅ] ᴏʀ [ᴅɪsᴀʙʟᴇ] : ᴄᴏɴɴᴇᴄᴛ ᴄʜᴀɴɴᴇʟ ᴛᴏ ᴀ ɢʀᴏᴜᴩ ᴀɴᴅ sᴛᴀʀᴛs sᴛʀᴇᴀᴍɪɴɢ ᴛʀᴀᴄᴋs ʙʏ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴄᴏᴍᴍᴀɴᴅs sᴇɴᴛ ɪɴ ɢʀᴏᴜᴩ.
+"""
+
+HELP_7 = """
+ɢʟᴏʙᴀʟ ʙᴀɴ ғᴇᴀᴛᴜʀᴇ [ᴏɴʟʏ ғᴏʀ sᴜᴅᴏᴇʀs] :
+
+🔸/gban [Kullanıcı adı veya bir kullanıcıya yanıt] - Botun sunduğu sohbetten bir kullanıcıyı Gban ve botunuzu kullanmasını engelleyin.
+🔸/ungban [Kullanıcı adı veya bir kullanıcıya yanıt] - Bir kullanıcıyı Bot'un gbanlı Listesinden çıkarın ve onun botunuzu kullanmasına izin verin
+🔸/gbannedusers - Gbanlı Kullanıcı Listelerini Kontrol Edin
+"""
+HELP_8 = """
+**LOOP STREAM:**
+
+ʟᴏᴏᴘ sᴛʀᴇᴀᴍ :
+
+sᴛᴀʀᴛs sᴛʀᴇᴀᴍɪɴɢ ᴛʜᴇ ᴏɴɢᴏɪɴɢ sᴛʀᴇᴀᴍ ɪɴ ʟᴏᴏᴘ
+
+/tekrar - /loop [enable/disable] : ᴇɴᴀʙʟᴇs/ᴅɪsᴀʙʟᴇs ᴛᴇᴋʀᴀʀʟᴀɴᴍᴀʏı ᴋᴀᴘᴀᴛıᴘ ᴀᴄ‌ᴀʙɪʟɪʀsɪɴ
+/tekrar - /loop [1, 2, 3, ...] : ɢɪʀᴅɪɢ‌ɪɴɪᴢ ʀᴀᴋᴀᴍ ᴋᴀᴅᴀʀ ᴘᴀʀᴄ‌ᴀ ᴛᴇʀᴋᴀʀʟᴀɴıʀ
+♨️🔸(örneğin: /döngü 4 - Parçayı 4 kez tekrarlar.)
+"""
+HELP_9 = """
+🔸/reboot - Botunuzu yeniden başlatın. 
+🔸/update - Botu Güncelle.
+🔸/speedtest - Sunucu hızlarını kontrol edin
+🔸/bakim [enable / disable] 
+🔸/logger [enable / disable] - Bot, logger grubunda aranan sorguları günlüğe kaydeder.
+🔸/get_log [Hat Sayısı] - Get log of your bot from heroku or vps. Works for both.
+🔸/autoend [enable|disable] - Hiç kimse dinlemiyorsa 3 dakika sonra otomatik akışı sonlandır özelliğini etkinleştirin.
+"""
+HELP_10 = """
+ᴘɪɴɢ & sᴛᴀᴛs :
+
+/start : Müzik botunu başlatırᴛ.
+/help : ɢᴇᴛ ʜᴇʟᴩ ᴍᴇɴᴜ ᴡɪᴛʜ ᴇxᴩʟᴀɴᴀᴛɪᴏɴ ᴏғ ᴄᴏᴍᴍᴀɴᴅs.
+/ping : sʜᴏᴡs ᴛʜᴇ ᴩɪɴɢ ᴀɴᴅ sʏsᴛᴇᴍ sᴛᴀᴛs ᴏғ ᴛʜᴇ ʙᴏᴛ.
+🔸/aktifses - Botta aktif sesli sohbetleri kontrol edin.
+🔸/aktifvideo - Botta aktif görüntülü aramaları kontrol edin.
+🔸/stats - Bot İstatistiklerini Kontrol Edin.
+"""
+HELP_11 = """
+ᴩʟᴀʏ ᴄᴏᴍᴍᴀɴᴅs :
+
+v : sᴛᴀɴᴅs ғᴏʀ ᴠɪᴅᴇᴏ ᴩʟᴀʏ.
+force : sᴛᴀɴᴅs ғᴏʀ ғᴏʀᴄᴇ ᴩʟᴀʏ.
+
+▪️ /voynat - Video Oynatır.
+/oynat /voynat /play - /vplay : sᴛᴀʀᴛs sᴛʀᴇᴀᴍɪɴɢ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.
+♨️🔸 /oynat komutu aynı zamanda canlı yayında destekler.(örnek: /oynat kralfm canlı)
+/playforce ᴏʀ /vplayforce : Sesli sohbette çalınan parçayı durdurur ve sırayı bozmadan temizlemeden aranan parçayı anında çalmaya başlar.
+"""
+HELP_12 = """
+**SHUFFLE QUEUE:**
+
+/shuffle : Sıradaki parçaları karıştırır.
+/queue : Karıştırılmış sırayı gösterir.
+"""
+
+HELP_13 = """
+**SEEK STREAM:**
+
+/seek [saniye cinsinden süre] : Yayını belirtilen süreye atlar.
+/seekback [saniye cinsinden süre] : Yayını belirtilen süre kadar geri alır.
+"""
+HELP_14 = """
+**SONG DOWNLOAD:**
+
+/song veya /bul [şarkı adı/YouTube URL] : YouTube'dan herhangi bir parçayı MP3 veya MP4 formatında indirir.
+"""
+HELP_15 = """
+**SPEED COMMANDS:**
+
+Ongoing stream'un çalma hızını kontrol edebilirsiniz. [Yalnızca yöneticiler]
+
+/speed veya /playback : Grubun ses çalma hızını ayarlamak için.
+/cspeed veya /cplayback : Kanalın ses çalma hızını ayarlamak için.
+"""
